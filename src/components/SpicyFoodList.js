6968d8b6dc6decbd86758f3c0cbfba7d952260dc
Selin Su Varol <selinsuvarol@gmail.com>
2022-06-07 12:23:52 +0300

    completed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,19 +6,39 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
     console.log(newFood);
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
-    </li>
-  ));
+  const [filterBy, setFilterBy] = useState("All");
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+const foodsToDisplay = foods.filter((food) => {
+  if (filterBy === "All") {
+    return true;
+  } else {
+    return food.cuisine === filterBy;
+  }
+});
+const foodList = foodsToDisplay.map((food) => (
+  <li key={food.id}>
+    {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+  </li>
+));
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+      <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
     </div>
   );
 }
